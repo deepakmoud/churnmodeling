43421ccb75b1d8f78e5528ddc21cca211044cd2d
lakshay1011 <83716870+lakshay1011@users.noreply.github.com>
2022-08-10 17:08:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,4 +66,5 @@ def predict():
     return render_template('index.html', prediction_text='Model  has predicted : {}'.format(message))
 
 
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
